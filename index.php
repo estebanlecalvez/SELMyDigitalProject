899b5a16827363095eaf7fbd6b7bc7c3035255e4
ESTEBAN LE CALVEZ <esteban.le-calvez@kurmi-software.com>
2018-03-08 16:46:32 +0100

    Comment faire une barre verticale comme les <hr> ???
    Comment faire une barre verticale comme les <hr> ???

--- index.php
+++ index.php
@@ -4,11 +4,11 @@
                         <div class="row" style="padding-top:20px">
                                 <div class="col"></div>
                                         <div class="border border-secondary jumbotron col"style="background-color:black; opacity:0.8;">
-                                                <h5 style="text-align : center;color : white; opacity:1;">
+                                                <h5 style="text-align : center;color : #0A71B4; opacity:1;">
                                                         Les avantages
                                                 </h5>
-                                                <p style="text-align : right;color : white; opacity:1;">Avantage 1</p>
-                                                <p style="text-align : right; color : white; opacity:1">Avantage 2</p>
+                                                <p style="text-align : right;color : #EED93A; opacity:1;">Avantage 1</p>
+                                                <p style="text-align : right; color : #EED93A; opacity:1">Avantage 2</p>
                                         </div>
                                 </div>
                         </div>
@@ -17,7 +17,7 @@
         <div class="container">
                 
                 <div class="border border-success jumbotron" style="margin-top:15px">
-                <h3 style="text-align : center"> Acces direct </h3>
+                <h3 style="text-align : center; color : #0A71B4"> Acces direct </h3>
                 <hr class="my-4">
                 <div class="row">
                         <div class="col"></div>
@@ -37,8 +37,8 @@
         </div>
 <div class="border border-success"style="background-color : lightgrey">
 <br/>
-<h3 style="text-align : center">
-                                                        <b>Comment ça marche.</b>
+<h3 style="text-align : center; color : #0A71B4">
+                                                        Comment ça marche.
                                                 </h3>
                                                 <br/>
                                                 <br/>
@@ -48,7 +48,7 @@
                                 <h4 style="text-align : center"> Je poste ma demande </h4>
                         </div>
                         <div class="col">
-                                <h4 style="text-align : center"> Quelqu'un me repond </h4>
+                                <h4 style="text-align : center"><b> Quelqu'un me repond </b></h4>
                         </div>
                         <div class="col">
                                 <h4 style="text-align : center"> Le service est rendu </h4>
