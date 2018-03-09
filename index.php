a3d82065dd60911e34f815954322097b7295ae7c
ESTEBAN LE CALVEZ <esteban.le-calvez@kurmi-software.com>
2018-03-09 09:41:00 +0100

    Image de la jolie Patricia 29 ans
    Image de la jolie Patricia 29 ans

--- index.php
+++ index.php
@@ -1,73 +1,73 @@
-<?php include('header.html');?>
-        <div style="background-image:url(img/img_banner.png);">
-                <div class="container">
-                        <div class="row" style="padding-top:20px">
-                                <div class="col"></div>
-                                        <div class="border border-secondary jumbotron col"style="background-color:black; opacity:0.8;">
-                                                <h5 style="text-align : center;color : #0A71B4; opacity:1;">
-                                                        Les avantages
-                                                </h5>
-                                                <p style="text-align : right;color : #EED93A; opacity:1;">Avantage 1</p>
-                                                <p style="text-align : right; color : #EED93A; opacity:1">Avantage 2</p>
-                                        </div>
-                                </div>
-                        </div>
-                </div>
+<?php include('header.html'); ?>
+<div style="background-image:url(img/img_banner.png);">
+    <div class="container">
+        <div class="row" style="padding-top:20px">
+            <div class="col"></div>
+            <div class="border border-secondary jumbotron col" style="background-color:black; opacity:0.8;">
+                <h5 style="text-align : center;color : #0A71B4; opacity:1;">
+                    Les avantages
+                </h5>
+                <p style="text-align : right;color : #EED93A; opacity:1;">Avantage 1</p>
+                <p style="text-align : right; color : #EED93A; opacity:1">Avantage 2</p>
+            </div>
         </div>
-        <div class="container">
-                
-                <div class="border border-success jumbotron" style="margin-top:15px">
-                <h3 style="text-align : center; color : #0A71B4"> Acces direct </h3>
-                <hr class="my-4">
-                <div class="row">
-                        <div class="col"></div>
-                        <div class="col">
-                                <img src="img/test_small_img.png">
-                        </div>
-                        <div class="col">
-                                <img src="img/test_small_img.png">
-                        </div>
-                        <div class="col">
-                                <img src="img/test_small_img.png">
-                        </div>
-                        <div class="col"></div>
+    </div>
+</div>
+</div>
+<div class="container">
 
-                </div>
-                </div>
-        </div>
-<div class="border border-success"style="background-color : lightgrey">
-<br/>
-<h3 style="text-align : center; color : #0A71B4">
-                                                        Comment ça marche.
-                                                </h3>
-                                                <br/>
-                                                <br/>
-                <div class="container">
-                        <div class="row">
-                        <div class="col">
-                                <h4 style="text-align : center"> Je poste ma demande </h4>
-                        </div>
-                        <div class="col">
-                                <h4 style="text-align : center"><b> Quelqu'un me repond </b></h4>
-                        </div>
-                        <div class="col">
-                                <h4 style="text-align : center"> Le service est rendu </h4>
-                        </div>
-                        
+    <div class="border border-success jumbotron" style="margin-top:15px">
+        <h3 style="text-align : center; color : #0A71B4"> Acces direct </h3>
+        <hr class="my-4">
+        <div class="row">
+            <div class="col"></div>
+            <div class="col">
+                <img src="img/test_small_img.png">
+            </div>
+            <div class="col">
+                <img src="img/test_small_img.png">
+            </div>
+            <div class="col">
+                <img src="img/test_small_img.png">
+            </div>
+            <div class="col"></div>
 
-                </div>
-                <br/>
-                                                <br/><br/>
-                                                <br/>
-                </div>
         </div>
-                
-        </div>
-        <div style="background-image:url(img/accueil_img1.png);">
-                
-        </div>
+    </div>
+</div>
+<div class="border border-dark" style="background-color : lightgrey">
+    <br/>
+    <h3 style="text-align : center; color : #0A71B4">
+        Comment ça marche.
+    </h3>
+    <br/>
+    <br/>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h4 style="text-align : center"> Je poste ma demande </h4>
+            </div>
+            <div class="col">
+                <h4 style="text-align : center"><b> Quelqu'un me repond </b></h4>
+            </div>
+            <div class="col">
+                <h4 style="text-align : center"> Le service est rendu </h4>
+            </div>
 
-   <?php include('footer.html');?>
+
+        </div>
+        <br/>
+        <br/><br/>
+        <br/>
+    </div>
+</div>
+
+</div>
+<div style="background-image:url(img/accueil_img1.png);">
+
+</div>
+
+<?php include('footer.html'); ?>
 </body>
 
 </html>
